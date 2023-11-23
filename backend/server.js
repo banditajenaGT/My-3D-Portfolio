eef0e856feef66e39b878c6930ad8cf7664311d6
banditajenaGT <banditajena2001@gmail.com>
2023-11-23 20:18:11 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,6 @@ app.get("*",(req,res)=>{
 })
 
  
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is listening on: http://localhost:${process.env.PORT}`)
 })
